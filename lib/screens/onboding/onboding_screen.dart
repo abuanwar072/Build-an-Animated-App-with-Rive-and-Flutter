725577f9061e250a40cc7ba9882f6a1921418b73
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2022-12-03 14:21:10 +0600

    btn done

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -1,16 +1,32 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-class OnbodingScreen extends StatelessWidget {
+class OnbodingScreen extends StatefulWidget {
   const OnbodingScreen({super.key});
+
+  @override
+  State<OnbodingScreen> createState() => _OnbodingScreenState();
+}
+
+class _OnbodingScreenState extends State<OnbodingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand,
         children: [
           Positioned(
             width: MediaQuery.of(context).size.width * 1.7,
@@ -34,6 +50,56 @@ class OnbodingScreen extends StatelessWidget {
               filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
               child: const SizedBox(),
             ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Spacer(),
+              GestureDetector(
+                onTap: () {
+                  _btnAnimationController.isActive = true;
+                },
+                child: SizedBox(
+                  height: 64,
+                  width: 236,
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.3),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: const SizedBox(),
+                        ),
+                      ),
+                      RiveAnimation.asset(
+                        "assets/RiveAssets/button.riv",
+                        controllers: [_btnAnimationController],
+                      ),
+                      Positioned.fill(
+                        top: 8,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Icon(CupertinoIcons.arrow_right),
+                            const SizedBox(width: 8),
+                            Text(
+                              "Start the course",
+                              style: Theme.of(context).textTheme.button,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
